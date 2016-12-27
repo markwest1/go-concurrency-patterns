30891e2eaeb136afe80fdb377ae20d3c924c5464
218024862 <mark.west@ge.com>
2016-12-27 16:27:31 -0600

    "Using channels" at 12:15/51:26

--- main.go
+++ main.go
@@ -7,15 +7,17 @@ import (
 )
 
 func main() {
-	go boring("Boring!")
-	fmt.Println("I'm listening.")
-	time.Sleep(2 * time.Second)
+	c := make(chan string)
+	go boring("boring!", c)
+	for i := 0; i < 5; i++ {
+		fmt.Printf("You say: %q\n", <-c) // Receive expression is just a value.
+	}
 	fmt.Println("You're boring; I'm leaving.")
 }
 
-func boring(msg string) {
+func boring(msg string, c chan string) {
 	for i := 0; ; i++ {
-		fmt.Println(msg, i)
+		c <- fmt.Sprintf("%s %d", msg, i) // Expression to be sent can be any suitable value.
 		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
