cd5bcba18a36ea052752101bcdf0a7796bea414d
218024862 <mark.west@ge.com>
2016-12-27 23:27:51 -0600

    "Daisy-chain" at 27:52/51:26

--- main.go
+++ main.go
@@ -1,45 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
 func main() {
-	quit := make(chan string)
-	c := boring("Joe", quit)
-
-	for {
-		select {
-		case s := <-c:
-			fmt.Println(s)
-		case <-quit:
-			cleanup()
-			quit <- "See you!"
-			return
-		}
+	const n = 1000000
+	leftmost := make(chan int)
+	right := leftmost
+	left := leftmost
+	for i := 0; i < n; i++ {
+		right = make(chan int)
+		go f(left, right)
+		left = right
 	}
+	go func(c chan int) {
+		c <- 1
+	}(right)
+	fmt.Println(<-leftmost)
 }
 
-func boring(msg string, q chan string) <-chan string {
-	c := make(chan string)
-	end := rand.Intn(10)
-
-	go func() { // We launch the goroutine from inside the funtion.
-		for i := 0; ; i++ {
-			c <- fmt.Sprintf("%s: %d", msg, i)
-			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-			if i == end {
-				q <- "Bye!"
-				fmt.Printf("Joe says: %q\n", <-q)
-			}
-		}
-	}()
-
-	return c
-}
-
-func cleanup() {
-	// Dummy function
+func f(left, right chan int) {
+	left <- 1 + <-right
 }
