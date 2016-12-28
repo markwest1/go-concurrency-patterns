63bf44b1406aa0ff53a297460aaadbef266f9cff
218024862 <mark.west@ge.com>
2016-12-28 00:03:18 -0600

    "Google Search 2.1" at 33:02/51:26

--- main.go
+++ main.go
@@ -36,9 +36,15 @@ func Google(query string) (results []Result) {
 	go func() { c <- Image(query) }()
 	go func() { c <- Video(query) }()
 
+	timeout := time.After(80 * time.Millisecond)
 	for i := 0; i < 3; i++ {
-		result := <-c
-		results = append(results, result)
+		select {
+		case result := <-c:
+			results = append(results, result)
+		case <-timeout:
+			fmt.Println("timed out")
+			return
+		}
 	}
 
 	return
