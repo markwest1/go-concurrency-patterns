38874e2d2a5ea91b69ce12af298b33c2f644eeb8
218024862 <mark.west@ge.com>
2016-12-27 18:54:34 -0600

    "Restoring sequencing." at 19:37/51:26

--- main.go
+++ main.go
@@ -6,16 +6,29 @@ import (
 	"time"
 )
 
+// Message provides a synchronizing channel type.
+type Message struct {
+	str  string
+	wait chan bool
+}
+
 func main() {
 	c := fanIn(boring("Joe"), boring("Ann"))
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-c)
+	for i := 0; i < 5; i++ {
+		msg1 := <-c
+		fmt.Println(msg1.str)
+
+		msg2 := <-c
+		fmt.Println(msg2.str)
+
+		msg1.wait <- true
+		msg2.wait <- true
 	}
 	fmt.Println("You're both boring; I'm leaving.")
 }
 
-func fanIn(input1, input2 <-chan string) <-chan string {
-	c := make(chan string)
+func fanIn(input1, input2 <-chan Message) <-chan Message {
+	c := make(chan Message)
 
 	go func() {
 		for {
@@ -32,13 +45,15 @@ func fanIn(input1, input2 <-chan string) <-chan string {
 	return c
 }
 
-func boring(msg string) <-chan string { // Returns receive-only channel of strings.
-	c := make(chan string)
+func boring(msg string) <-chan Message { // Returns receive-only channel of strings.
+	c := make(chan Message)
+	waitForIt := make(chan bool)
 
 	go func() { // We launch the goroutine from inside the funtion.
 		for i := 0; ; i++ {
-			c <- fmt.Sprintf("%s: %d", msg, i)
+			c <- Message{fmt.Sprintf("%s: %d", msg, i), waitForIt}
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+			<-waitForIt
 		}
 	}()
 
